040bd1a1c22b8341e7a8397867088aee53459783
Suryadevara Karpooravally <karpuravalli7@gmail.com>
2019-03-11 22:30:20 +0530

    Maven Project Added to local repository

---
+++ mavenproject1/src/main/java/MySampleClass/MySampleClass.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MySampleClass;
+
+/**
+ *
+ * @author MURALI
+ */
+public class MySampleClass {
+    public static void main(String[] args){
+        System.out.println("My Sample Java Class");
+    } 
+}
